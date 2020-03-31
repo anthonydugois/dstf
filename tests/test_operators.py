d919cece0ae0fbe0446633f9f2f49dc2f4ec2aaf
Anthony Dugois <anthony.dugois@protonmail.com>
2020-03-31 11:40:52 +0200

    Added tests for operators

---
+++ tests/test_operators.py
@@ -0,0 +1,25 @@
+from dstf import *
+
+
+def test_apply__append():
+    task = Task("t0")
+    node = Node("n0")
+    sched = Schedule().update(AppendOperator(task, 0, {node: 10}))
+
+    assert task in sched
+    assert len(sched[task]) == 1
+    assert sched[task][0].start_time == 0
+    assert sched[task][0].proc_times == {node: 10}
+
+
+def test_apply__preempt():
+    task = Task("t0")
+    node = Node("n0")
+    sched = Schedule().update(AppendOperator(task, 0, {node: 10})).update(PreemptOperator(task, 5, {node: 10}))
+
+    assert task in sched
+    assert len(sched[task]) == 2
+    assert sched[task][0].start_time == 0
+    assert sched[task][0].proc_times == {node: 5}
+    assert sched[task][1].start_time == 5
+    assert sched[task][1].proc_times == {node: 10}
