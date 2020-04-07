87a5f076c28de31850eafe9de2f6805ca2697744
Anthony Dugois <anthony.dugois@protonmail.com>
2020-04-07 15:29:51 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(name="dstf",
-                 version="0.0.2",
+                 version="0.0.3",
                  packages=setuptools.find_packages(),
                  url="https://github.com/anthonydugois/dstf",
                  author="Anthony Dugois",
